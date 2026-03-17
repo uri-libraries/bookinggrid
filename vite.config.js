5d87c3f9e0e8e13b2038d618f05a9ae8216182eb
ktzrbn <katzir.bn@gmail.com>
2026-03-17 09:50:57 -0400

    updates

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+console.log('ENV:', import.meta.env);
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
