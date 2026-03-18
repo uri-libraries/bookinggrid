9bc5abfb476f30d542031b572c113874e4ba8480
ktzrbn <katzir.bn@gmail.com>
2026-03-18 12:19:02 -0400

    complete

--- vite.config.js
+++ vite.config.js
@@ -1,36 +1,46 @@
-import { defineConfig } from 'vite'
+
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-console.log('VITE_LIBCAL_BASE_URL:', process.env.VITE_LIBCAL_BASE_URL);
-console.log('VITE_LIBCAL_OAUTH_URL:', process.env.VITE_LIBCAL_OAUTH_URL);
-console.log('NODE_ENV:', process.env.NODE_ENV);
-console.log('MODE:', process.env.MODE);
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  const env = loadEnv(mode, process.cwd(), '')
+  console.log('VITE_LIBCAL_BASE_URL:', env.VITE_LIBCAL_BASE_URL)
+  console.log('VITE_LIBCAL_OAUTH_URL:', env.VITE_LIBCAL_OAUTH_URL)
+  console.log('NODE_ENV:', env.NODE_ENV)
+  console.log('MODE:', mode)
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  base: '/',
-  build: {
-    assetsInlineLimit: 0 // Always copy assets, never inline as base64
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://uri.libcal.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '/api/1.1'),
-        configure: (proxy, options) => {
-          proxy.on('proxyReq', (proxyReq, req, res) => {
-            if (req.headers.authorization) {
-              proxyReq.setHeader('Authorization', req.headers.authorization);
-            }
-          });
-        }
+  // https://vitejs.dev/config/
+  return {
+    plugins: [vue()],
+    base: '/',
+    resolve: {
+      alias: {
+        '@': '/src',
       },
-      '/oauth': {
-        target: 'https://uri.libcal.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/oauth/, '/1.1/oauth')
+    },
+    build: {
+      assetsInlineLimit: 0 // Always copy assets, never inline as base64
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: 'https://uri.libcal.com',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '/api/1.1'),
+          configure: (proxy, options) => {
+            proxy.on('proxyReq', (proxyReq, req, res) => {
+              if (req.headers.authorization) {
+                proxyReq.setHeader('Authorization', req.headers.authorization);
+              }
+            });
+          }
+        },
+        '/oauth': {
+          target: 'https://uri.libcal.com',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/oauth/, '/1.1/oauth')
+        }
       }
     }
   }
